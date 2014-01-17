d657f5fbad0f41175075553862f6c791ce483078
Chris Thorpe <chris.thorpe@jaggeree.com>
2014-01-17 17:07:13 +0000

    Removing the last sample comment

--- Basic motor control/Basic_Motor_Control/Basic_Motor_Control.ino
+++ Basic motor control/Basic_Motor_Control/Basic_Motor_Control.ino
@@ -1,7 +1,5 @@
 /*
 
-  We know how to comment now, so let's write some code.
-
   This code is based on some existing tutorials, in particular one on Adafruit's Learning platform
 
   http://learn.adafruit.com/adafruit-arduino-lesson-13-dc-motors/overview
@@ -18,5 +16,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly: 
-  
+
 }
