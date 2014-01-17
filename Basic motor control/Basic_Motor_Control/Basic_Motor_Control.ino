ae4033538b0b6b1eb0032512ee397f44c59937e4
Chris Thorpe <chris.thorpe@jaggeree.com>
2014-01-17 17:51:45 +0000

    Starting to introduce the concept of variables and constants
    It's not good practice to "hard wire" variables, what if we wanted to
    change pins.
    
    So let's create some constants. Contstants are just variables which
    don't change.

--- Basic motor control/Basic_Motor_Control/Basic_Motor_Control.ino
+++ Basic motor control/Basic_Motor_Control/Basic_Motor_Control.ino
@@ -6,6 +6,16 @@
 
 */
 
+/*
+
+  It's not good practice to "hard wire" variables, what if we wanted to change pins.
+
+  So let's create some constants. Contstants are just variables which don't change.
+
+*/
+
+
+const int MOTOR_PIN = 3;
 
 
 
